e72737a00be362cdd55cc695998b14710c39ed1e
cortezalberto <cortezalberto@gmail.com>
2024-09-14 15:01:48 -0300

    Cuarto commit

--- src/main/java/com/example/inicial1/controllers/PersonaController.java
+++ src/main/java/com/example/inicial1/controllers/PersonaController.java
@@ -2,6 +2,9 @@ package com.example.inicial1.controllers;
 
 import com.example.inicial1.dtos.PersonaDto;
 import com.example.inicial1.entities.Persona;
+import com.example.inicial1.services.PersonaServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,38 +13,71 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/personas")
 public class PersonaController {
+ //  @Autowired
+//PersonaServices servicio;
+
+    @GetMapping("")
+    public ResponseEntity<?> getAll(){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).
+                    body("Busqué todos los datos");
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente más tarde\"}");
+        }
+    }
+
 
 
     @GetMapping("/{id}")
-    public ResponseEntity<String> getPersona(@PathVariable Long id) {
-        // Lógica para obtener la persona por ID
-        String response = "Persona con ID: " + id;
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<?> getOne(@PathVariable Long id){
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body("Busqué esta persona por Id:" + id);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("{\"error\":\"Error, por favor intente más tarde\"}");
+        }
     }
 
-    @GetMapping
-    public ResponseEntity<String> getAllPersonas() {
-        // Lógica para obtener todas las personas
-        String response = "Lista de todas las personas";
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+    @PostMapping("")
+    public ResponseEntity<?> save(@RequestBody Persona entity){
 
-    @PostMapping
-    public ResponseEntity<String> createPersona(@RequestBody PersonaDto persona) {
-        // Lógica para crear una nueva persona
-        String response = "Persona creada con éxito";
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        System.out.println("Estos datos los tomo del cuerpo del Formulario");
+        System.out.println("Nombre :" + entity.getNombre());
+        System.out.println("Nombre :" + entity.getApellido());
+
+
+
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body("Grabé los datos anteriores");
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente más tarde\"}");
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updatePersona(@PathVariable Long id, @RequestBody PersonaDto persona) {
-        System.out.println("el nombre a actualizae es :" + persona.getNombre());
-
-
-        // Lógica para actualizar la persona por ID + id
-        String response = "Persona actualizada con éxito" + id;
-        return new ResponseEntity<>(response, HttpStatus.OK);
+    public ResponseEntity<?> update(@PathVariable Long id, @RequestBody Persona entity){
+        System.out.println("EL ID LO TOMO DE LA URL");
+        System.out.println("Nombre :" + entity.getId());
+        System.out.println("Estos datos los tomo del cuerpo del Formulario");
+        System.out.println("Nombre :" + entity.getNombre());
+        System.out.println("Apellido :" + entity.getApellido());
+        try{
+            return ResponseEntity.status(HttpStatus.OK).body("Actualicé los datos anteriores");
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente más tarde\"}");
+        }
     }
 
-    // Puedes agregar el método DELETE si lo necesitas
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id){
+        try{
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Eliminé el registro" + id);
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error, por favor intente más tarde\"}");
+        }
+    }
 }
